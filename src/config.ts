80deb075f5a231a8b82d52534b8c4b89352d3c24
Tanisha Sabherwal <tanisha.sabherwal@gameskraft.com>
2024-10-02 15:57:06 +0530

    added files

---
+++ src/config.ts
@@ -0,0 +1,25 @@
+export interface Config {
+    projectID: string;
+    extensionID: string;
+    location: string;
+    iosBundleID: string;
+    iosTeamID: string;
+    androidBundleID: string;
+    androidSHA?: string;
+    androidScheme?: string;
+    domainPostfix: string;
+  }
+  
+  const config: Config = {
+    projectID: process.env.PROJECT_ID || '',
+    extensionID: process.env.EXT_INSTANCE_ID || '',
+    location: process.env.LOCATION || 'us-west1',
+    iosBundleID: process.env.IOS_BUNDLE_ID || '',
+    iosTeamID: process.env.IOS_TEAM_ID || '',
+    androidBundleID: process.env.ANDROID_BUNDLE_ID || '',
+    androidSHA: process.env.ANDROID_SHA || '',
+    androidScheme: process.env.ANDROID_SCHEME || '',
+    domainPostfix: process.env.DOMAIN_POSTFIX || 'flowlinks',
+  };
+  
+  export default config;
---
+++ src/index.ts
@@ -0,0 +1,78 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import * as express from 'express';
+import * as fs from "fs";
+import * as path from 'path';
+
+// Initialize Firebase Admin SDK
+admin.initializeApp();
+
+// Initialize Express app
+const app = express();
+
+// Set up Firebase Cloud Function
+export const api = functions.https.onRequest(app);
+
+app.get('/.well-known/apple-app-site-association', (req, res) => {
+    const applicationID = `${'FGSV552D'}.${'com.example.myawesomeapp'}`
+    // Example: FGSV552D.com.example.myawesomeapp
+
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.write(JSON.stringify({
+        "applinks": {
+            "apps": [],
+            "details": [{
+                "appID": applicationID,
+                "paths": [
+                    "*",
+                ],
+            }]
+        },
+        "webcredentials": {
+            "apps": [
+                applicationID
+            ]
+        }
+    }))
+    res.end();
+});
+
+app.get('/.well-known/assetlinks.json', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.write(JSON.stringify(
+        [{
+            "relation": [
+                "delegate_permission/common.handle_all_urls"
+            ],
+            "target": {
+                "namespace": "android_app",
+                "package_name": "YOUR_PACKAGE_NAME",
+                "sha256_cert_fingerprints": [
+                    "YOUR_SHA256_FINGERPRINT"
+                ]
+            }
+        }]
+    ))
+    res.end();
+});
+
+//@ts-ignore
+app.get('*', (req, res, next) => {
+    // Define values
+    const title = 'My Amazing Application';
+    const subtitle = 'Find out more about my app...';
+    const image = 'https://.../your-app-banner.jpg';
+
+    // Load HTML template
+    const templatePath = path.join(__dirname, './assets/html/index.html');
+
+    // Replace handles with content
+    var source = fs.readFileSync(templatePath, { encoding: 'utf-8' })
+        //@ts-ignore
+        .replaceAll('{{title}}', title)
+        .replaceAll('{{subtitle}}', subtitle)
+        .replaceAll('{{image}}', image);
+
+    // Return the webpage
+    return res.send(source);
+});
